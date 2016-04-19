518849ff6e2f3a34a20ad9add5863f0992726762
Brendon Hollingsworth <tucaeg@yahoo.com>
2016-04-18 21:17:06 -0700

    Clean up the python code

--- DES.py
+++ DES.py
@@ -2,29 +2,20 @@ from CipherInterface import CipherInterface
 
 class DES(CipherInterface):
 	def __init__(self):
-		self.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 		self.key = ""
 
 	def setKey(self, keyString):
-		self.key = keyString
+		# code goes here
 		return True
 
 	def encrypt(self, plaintext):
-		plaintext = plaintext.upper()
 		ciphertext = ""
-		for char in plaintext:
-			if char in self.alphabet:
-				pos = self.alphabet.find(char)
-				ciphertext += self.alphabet[(pos + int(self.key)) % 26]
+		# code stuff goes here
 
 		return ciphertext
 
 	def decrypt(self, ciphertext):
-		ciphertext = ciphertext.upper()
 		plaintext = ""
-		for char in ciphertext:
-			if char in self.alphabet:
-				pos = self.alphabet.find(char)
-				plaintext += self.alphabet[(pos - int(self.key)) % 26]
+		# code stuff goes here
 
 		return plaintext
--- RSA.py
+++ RSA.py
@@ -2,35 +2,20 @@ from CipherInterface import CipherInterface
 
 class RSA(CipherInterface):
 	def __init__(self):
-		self.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 		self.key = ""
 
 	def setKey(self, keyString):
-		self.key = keyString.upper()
+		# code goes here
 		return True
 
 	def encrypt(self, plaintext):
-		plaintext = plaintext.upper()
 		ciphertext = ""
-		index = 0
-		for char in plaintext:
-			if char in self.alphabet:
-				pos = self.alphabet.find(char)
-				keyPos = self.alphabet.find(self.key[index % len(self.key)])
-				ciphertext += self.alphabet[(pos + keyPos) % 26]
-				index += 1
+		# code goes here
 
 		return ciphertext
 
 	def decrypt(self, ciphertext):
-		ciphertext = ciphertext.upper()
 		plaintext = ""
-		index = 0
-		for char in ciphertext:
-			if char in self.alphabet:
-				pos = self.alphabet.find(char)
-				keyPos = self.alphabet.find(self.key[index % len(self.key)])
-				plaintext += self.alphabet[(pos - keyPos) % 26]
-				index += 1
+		# code goes here
 
 		return plaintext
